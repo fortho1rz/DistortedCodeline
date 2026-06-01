22b1e727401eec2b712dbc5d8ae9c4afe1c4eadf
fortho1rz <holyagayao@gmail.com>
2026-06-01 20:12:00 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fortho1rz/DistortedGames/refs/heads/main/loader.lua")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fortho1rz/DistortedGames/refs/heads/main/loader.lua"))()
